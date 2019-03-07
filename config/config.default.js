c1f3700a40ca640d90904a654ad885c0fb616c79
lzuwen <lzuwen@linewell.com>
2019-03-07 10:55:51 +0800

    demo初始化

---
+++ config/config.default.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = appInfo => {
+    const config = exports = {};
+
+    // use for cookie sign key, should change to your own and keep security
+    config.keys = appInfo.name + '_1513765449219_5858';
+
+    config.view = {
+        root: path.join(appInfo.baseDir, 'app/view'),
+        mapping: {
+            '.html': 'nunjucks',
+        },
+
+    };
+
+    config.assets = {
+        publicPath: '/public',
+        devServer: {
+            port: 8000,
+            // autoPort: true,
+            command: 'umi dev --port=8000',
+            env: {
+                APP_ROOT: path.join(__dirname, '../app/web'),
+                BROWSER: 'none',
+                SOCKET_SERVER: 'http://127.0.0.1:8000',
+            },
+            debug: true,
+        },
+    };
+
+    config.security = {
+        csrf: false,
+    };
+
+    return config;
+};
